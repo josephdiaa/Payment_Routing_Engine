e79801482cbda7ad036b2d2acc7b70208af6f6f1
joseph diaa <joseph.diaa@gmail.com>
2026-05-18 00:06:55 +0300

    feat(US-01): initialize Spring Boot project, package structure, health endpoint

--- src/main/java/com/example/payment_routing_engine/common/response/ApiResponse.java
+++ src/main/java/com/example/payment_routing_engine/common/response/ApiResponse.java
@@ -1,4 +1,22 @@
 package com.example.payment_routing_engine.common.response;
 
-public class ApiResponse {
+
+
+import java.time.Instant;
+
+public record ApiResponse<T>(
+        boolean success,
+        T data,
+        String message,
+        Instant timestamp
+) {
+    public static <T> ApiResponse<T> ok(T data){
+        return new ApiResponse<>(true,data,"ok",Instant.now());
+    }
+    public static <T> ApiResponse<T> ok(T date,String message){
+        return new ApiResponse<>(true,date, message,Instant.now());
+    }
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, null, message, Instant.now());
+    }
 }
--- src/main/java/com/example/payment_routing_engine/common/web/HealthController.java
+++ src/main/java/com/example/payment_routing_engine/common/web/HealthController.java
@@ -1,4 +1,15 @@
 package com.example.payment_routing_engine.common.web;
 
+import com.example.payment_routing_engine.common.response.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class HealthController {
+
+    @GetMapping("/api/health")
+    public ApiResponse<Object> checkHealth(){
+        return  ApiResponse.ok("UP","ok");
+    }
 }
