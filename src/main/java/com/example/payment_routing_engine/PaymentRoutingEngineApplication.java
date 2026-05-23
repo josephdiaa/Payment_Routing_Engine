e84ae87b21d815f340c01d5fb5693d502646b269
joseph diaa <joseph.diaa@gmail.com>
2026-05-23 10:46:42 +0300

    feat(US-24): implement JWT security, Authentication endpoints, and seed admin user

--- src/main/java/com/example/payment_routing_engine/PaymentRoutingEngineApplication.java
+++ src/main/java/com/example/payment_routing_engine/PaymentRoutingEngineApplication.java
@@ -1,7 +1,14 @@
 package com.example.payment_routing_engine;
 
+import com.example.payment_routing_engine.modules.user.domain.User;
+import com.example.payment_routing_engine.modules.user.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.Instant;
 
 @SpringBootApplication
 public class PaymentRoutingEngineApplication {
